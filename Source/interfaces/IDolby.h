8a36ae19b52aea4c1a9b15ff4c5125340d713f12
krp97 <k.plata576@gmail.com>
2020-06-19 14:13:27 +0200

    Added IDolby interface

---
+++ Source/interfaces/IDolby.h
@@ -0,0 +1,43 @@
+/*
+ * If not stated otherwise in this file or this component's LICENSE file the
+ * following copyright and licenses apply:
+ *
+ * Copyright 2020 RDK Management
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include "Module.h"
+
+namespace WPEFramework {
+namespace Exchange {
+
+    struct IDolby {
+        enum { ID = ID_DOLBY };
+
+        virtual ~IDolby(){};
+
+        enum Output {
+            AUTO,
+            ATMOS_PASS_THROUGH,
+            DIGITAL_PASS_THROUGH,
+            DIGITAL_PCM
+        };
+
+        virtual void Output(IDolby::Output type) = 0;
+        virtual IDolby::Output Output() = 0;
+    };
+}
+}
